b29852747f6928775b13a256b0f0534debcadb3e
Aneervan Ray <aneervan.raja2000@gmail.com>
2026-01-01 11:20:56 +0530

    Initial commit

---
+++ src/main/java/com/anee/module4_prod_ready_features_own/prod_ready_features_own/ProdReadyFeaturesOwnApplication.java
@@ -0,0 +1,13 @@
+package com.anee.module4_prod_ready_features_own.prod_ready_features_own;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdReadyFeaturesOwnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdReadyFeaturesOwnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anee/module4_prod_ready_features_own/prod_ready_features_own/ProdReadyFeaturesOwnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anee.module4_prod_ready_features_own.prod_ready_features_own;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdReadyFeaturesOwnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
